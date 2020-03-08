c3174182ab390e28349f3f8c84bb5a7866771356
WhileTrueThenDream <whiletruethendream@gmail.com>
2020-03-08 08:42:16 +0100

    Create sm_write.c

---
+++ sm_write.c
@@ -0,0 +1,43 @@
+/*
+   It writes to a previously created memory object "/myMemoryObj"
+   to be compiled with "-lrt"
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+ 
+#define SMOBJ_NAME  "/myMemoryObj"
+
+int main(void)
+{
+   int fd;
+   char buf[] = "Hello,this is writting process";
+   char *ptr;
+   
+   fd = shm_open (SMOBJ_NAME,  O_RDWR  , 00200); /* open s.m object*/
+   if(fd == -1)
+   {
+       printf("Error file descriptor %s\n", strerror(errno));
+	   exit(-1);
+   }
+   
+   ptr = mmap(NULL, sizeof(buf), PROT_WRITE, MAP_SHARED, fd, 0);
+   if(ptr == MAP_FAILED)
+   {
+      printf("Map failed in write process: %s\n", strerror(errno));
+      exit(-1);
+   }
+   
+   memcpy(ptr,buf, sizeof(buf));
+   printf("%d \n", (int)sizeof(buf));
+   close(fd);
+   
+   return 0;
+}

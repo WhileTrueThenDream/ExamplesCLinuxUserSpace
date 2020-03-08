4029d5a639fb1098ee942a16e0b9b1fb1a91002e
WhileTrueThenDream <whiletruethendream@gmail.com>
2020-03-08 09:10:33 +0100

    exit code to be 1 --> 1 - Catchall for general errors.
    exit code to be 1 --> 1 - Catchall for general errors.

--- sm_read.c
+++ sm_read.c
@@ -25,19 +25,19 @@ int main(void)
    if(fd == -1)
    {
        printf("Error file descriptor %s\n", strerror(errno));
-	   exit(-1);
+	   exit(1);
    }
    
    if(fstat(fd, &shmobj_st) == -1)
    {
        printf(" error fstat \n");
-	   exit(-1);
+	   exit(1);
    }
    ptr = mmap(NULL, shmobj_st.st_size, PROT_READ, MAP_SHARED, fd, 0);
    if(ptr == MAP_FAILED)
    {
       printf("Map failed in read process: %s\n", strerror(errno));
-      exit(-1);
+      exit(1);
    }
    
    printf("%s \n", ptr);

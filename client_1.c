324f7a16871b83857bd40f42cef5b47d5349ccd0
WhileTrueThenDream <whiletruethendream@gmail.com>
2021-01-20 21:29:17 +0100

    Create client_1.c
    file added

---
+++ client_1.c
@@ -0,0 +1,68 @@
+/*************************************************************************************/
+/* @file    client_1.c                                                               */
+/* @brief   This clients connects,                                                   */
+/*          sends a text, reads what server and disconnects                          */
+/*************************************************************************************/
+
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <unistd.h>
+
+#define SERVER_ADDRESS  "192.168.0.21"     /* server IP */
+#define PORT            8080 
+
+/* Test sequences */
+char buf_tx[] = "Hello server. I am a client";      
+char buf_rx[100];                     /* receive buffer */
+ 
+ 
+/* This clients connects, sends short sequences in while loop and never disconnects */
+int main() 
+{ 
+    int sockfd; 
+    struct sockaddr_in servaddr; 
+    
+    /* Socket creation */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) 
+    { 
+        printf("CLIENT: socket creation failed...\n"); 
+        return -1;  
+    } 
+    else
+    {
+        printf("CLIENT: Socket successfully created..\n"); 
+    }
+    
+    
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    /* assign IP, PORT */
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = inet_addr( SERVER_ADDRESS ); 
+    servaddr.sin_port = htons(PORT); 
+  
+    /* try to connect the client socket to server socket */
+    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) 
+    { 
+        printf("connection with the server failed...\n");  
+        return -1;
+    } 
+    
+    printf("connected to the server..\n"); 
+  
+    /* send test sequences*/
+    write(sockfd, buf_tx, sizeof(buf_tx));     
+    read(sockfd, buf_rx, sizeof(buf_rx));
+    printf("CLIENT:Received: %s \n", buf_rx);
+   
+       
+    /* close the socket */
+    close(sockfd); 
+} 
